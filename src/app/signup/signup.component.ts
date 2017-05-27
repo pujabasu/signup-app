664ee9626b99a6cd116b91ec5dbc4a48c48fa68e
pujabasu <basupuja2007@gmail.com>
2017-05-27 23:00:11 +0200

    Refactoring

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
   constructor() { }
   userName: string;
+  tempUsr:string;
   password: string;
   validEmail:string;
   valid: boolean=true;
@@ -17,7 +18,6 @@ export class SignupComponent implements OnInit {
   temp:any[];
   max:any;
   hyperlink:string="";
- // validPwd: string;
   isButtonDisable:boolean=true;
   ngOnInit() {
   }
@@ -26,24 +26,20 @@ export class SignupComponent implements OnInit {
     this.valid=true;
   }
   enableButton(e){
-   // this.validEmail="";
-   // this.emptyUserName=false;
-   // this.valid=false;
    this.hyperlink="";
     if((this.userName && this.userName!='')){
+      this.tempUsr=this.userName.toLowerCase();
       this.emptyUserName=false;
-      // this.isButtonDisable=false;
       let regx=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
-      if(!regx.test(this.userName)){
+      if(!regx.test(this.tempUsr)){
         this.valid=false;
         this.validEmail="UserName is not complete.";
       }
       else {
-       // this.validEmail="did you mean";
-         var rateSymbolPos= this.userName.indexOf("@");
+         var rateSymbolPos= this.tempUsr.indexOf("@");
          
          this.validEmailList.some((element,index)=>{
-           if(element===this.userName.substring(rateSymbolPos+1,this.userName.length)){
+           if(element===this.tempUsr.substring(rateSymbolPos+1,this.tempUsr.length)){
              this.valid=true;
               return true;
            }
@@ -55,17 +51,13 @@ export class SignupComponent implements OnInit {
            this.temp=[];
            for(let count=0;count<this.validEmailList.length;count++){
              let match=0;
-             for(let index=0;index<this.userName.substring(rateSymbolPos+1,this.userName.length).length;index++){
-              if(this.validEmailList[count].indexOf(this.userName.substring(rateSymbolPos+1,this.userName.length)[index])>=0){
+             for(let index=0;index<this.tempUsr.substring(rateSymbolPos+1,this.tempUsr.length).length;index++){
+              if(this.validEmailList[count].indexOf(this.tempUsr.substring(rateSymbolPos+1,this.tempUsr.length)[index])>=0){
                 match++;
               }
              }
             if(match>0){
                  this.temp.push({'matchedKey':match,'mail': this.validEmailList[count]});
-              // if( match/this.validEmailList[count].length * 100 >= 60)
-              //   this.validEmail="Do you mean "+ this.userName.substring(0,rateSymbolPos)+"@"+this.validEmailList[count];
-              // else
-              //   this.validEmail="Please check the username entered.";
             }
            }
 
@@ -76,23 +68,7 @@ export class SignupComponent implements OnInit {
               })
 
                this.checkIfUsernameIsValid(this.temp[0].matchedKey,this.temp[0].mail,rateSymbolPos);
-             
-            //  if( this.max[0].matchedKey/this.max[0].mail.length * 100 >= 60){
-            //    this.hyperlink=this.userName.substring(0,rateSymbolPos)+"@"+this.max[0].mail;
-            //    this.validEmail="Do you mean "+ "";
-            //  }
-            //   else{
-            //     this.hyperlink="";
-            //     this.validEmail="Please check the username entered.";
-            //   }
            }
-           
-          //  else if(this.temp.length==1){
-          //     if( this.temp[0].match/this.temp[0].mail.length * 100 >= 60)
-          //       this.validEmail="Do you mean "+ this.userName.substring(0,rateSymbolPos)+"@"+this.temp[0].mail;
-          //     else
-          //       this.validEmail="Please check the username entered.";
-          //  }
            else
                 this.validEmail="UserName entered is incorrect.";
          }
@@ -116,46 +92,12 @@ export class SignupComponent implements OnInit {
 
 checkIfUsernameIsValid(matched:number,checkStr:string,rateSymbolPos:number){
       if( matched/checkStr.length * 100 >= 60){
-          this.hyperlink=this.userName.substring(0,rateSymbolPos)+"@"+checkStr;
+          this.hyperlink=this.userName.toLowerCase().substring(0,rateSymbolPos)+"@"+checkStr;
           this.validEmail="Do you mean "+ "";
       }
       else{
         this.hyperlink="";
         this.validEmail="UserName is not valid";
       }
-}
- // submitResult(){
-      // let regx=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
-      // if(!regx.test(this.userName)){
-      //   this.validEmail="Please provide a valid email";
-      // }
-      // else {
-      //   this.validEmail="";
-      //  // this.validEmail="did you mean";
-      //    var rateSymbolPos= this.userName.indexOf("@");
-      //    let valid: boolean;
-      //    this.validEmailList.forEach((element,index)=>{
-      //      if(element===this.userName.substring(rateSymbolPos+1,this.userName.length)){
-      //         valid=true;
-      //      }
-      //    });
-      //    if(!valid){
-      //      for(let count=0;count<this.validEmailList.length;count++){
-      //        let match=0;
-      //        for(let index=0;index<this.userName.substring(rateSymbolPos+1,this.userName.length).length;index++){
-      //         if(this.validEmailList[count].includes(this.userName.substring(rateSymbolPos+1,this.userName.length)[index])){
-      //           match++;
-      //         }
-      //        }
-      //       if(match>0){
-      //         if( match/this.validEmailList[count].length * 100 >= 80)
-      //           this.validEmail="Did you mean "+ this.userName.substring(0,rateSymbolPos)+"@"+this.validEmailList[count];
-      //         else
-      //           this.validEmail="Please check the username entered.";
-      //         break;
-      //       }
-      //      }
-      //    }
-      // }
-   // }
+  }
 }
