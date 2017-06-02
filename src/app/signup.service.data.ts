09e2c38c80ab1004e4b80ed046113b8ceaa0f1ee
pujabasu <basupuja2007@gmail.com>
2017-06-02 15:08:10 +0200

    Deleting the file.

--- src/app/signup.service.data.ts
+++
@@ -1,9 +0,0 @@
-// using a hardcoded array containing the real and dummy domain names to be compared 
-// with the domain names provided in username to check any typo and
-// suggest the correct domain. In real world, this needs to fetched from DNS server.
-export const serviceData =[
-    'gmail.com',
-    'sweden.gov.se',
-    'yahoo.com',
-    'hotmail.com'
-];
